d0ef0bb1e6a9557968e6207ce0d039a9a62a07a9
Freypa24 <Freyptpersonal@gmail.com>
2025-02-09 12:14:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import request as request
+from selenium import webdriver
+from selenium.webdriver import chrome
+from bs4 import BeautifulSoup
+
+driver = chrome
+
+driver.get('https://philkotse.com/used-cars-for-sale')
